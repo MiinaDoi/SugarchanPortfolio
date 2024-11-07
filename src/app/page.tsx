c5ccf34c14d82b99fd699f670d8ccc995699f5fc
Sugar1201 <140928.miina@gmail.com>
2024-11-07 14:52:17 +0900

    Move Header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Header from "../components/header";
 import Hero from "../components/hero";
 
 export default function Home() {
@@ -10,7 +9,6 @@ export default function Home() {
         <meta name="description" content="A portfolio showcasing my work." />
       </Head>
       <div className="relative min-h-screen bg-white">
-        <Header />
         <Hero />
       </div>
     </>
